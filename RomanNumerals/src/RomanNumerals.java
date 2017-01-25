81b3ad6c0cf06942491ad772e1a0df805fcdd0e3
EA <emashliles@gmail.com>
2017-01-25 13:45:35 +0000

    Initial template.

--- RomanNumerals/src/RomanNumerals.java
+++ RomanNumerals/src/RomanNumerals.java
@@ -1,7 +1,2 @@
-package PACKAGE_NAME;
-
-/**
- * Created by emma-ashleyliles on 25/01/2017.
- */
 public class RomanNumerals {
 }
--- RomanNumerals/test/RomanNumeralsTest.java
+++ RomanNumerals/test/RomanNumeralsTest.java
@@ -1,8 +1,5 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Created by emma-ashleyliles on 25/01/2017.
- */
 class RomanNumeralsTest {
 
 }
