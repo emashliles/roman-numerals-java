297e3bb842a4e936d0b5e10c13e2ff44e9984fcb
EA <emashliles@gmail.com>
2017-01-25 14:18:32 +0000

    Added initial failing test

--- RomanNumerals/src/RomanNumerals.java
+++ RomanNumerals/src/RomanNumerals.java
@@ -1,2 +1,9 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class RomanNumerals {
+
+
+    public static String convert(int i) {
+        throw new NotImplementedException();
+    }
 }
--- RomanNumerals/test/RomanNumeralsTest.java
+++ RomanNumerals/test/RomanNumeralsTest.java
@@ -1,5 +1,13 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RomanNumeralsTest {
+
+    @Test
+    public void Converts0(){
+        String roman = RomanNumerals.convert(0);
+        assertEquals("", roman);
+    }
 
 }
