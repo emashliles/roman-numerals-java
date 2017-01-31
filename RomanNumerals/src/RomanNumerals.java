342f311901323a2439a7a96c8f8b067f801a9167
EA <emashliles@gmail.com>
2017-01-31 16:00:25 +0000

    Replace if statements with loop.

--- RomanNumerals/src/RomanNumerals.java
+++ RomanNumerals/src/RomanNumerals.java
@@ -1,54 +1,42 @@
+import com.sun.tools.javac.code.Scope;
+
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class RomanNumerals {
 
-    private HashMap<Integer, String> numerals;
+    private LinkedHashMap<Integer, String> numerals;
 
     public RomanNumerals() {
-        this.numerals = new HashMap<>();
+        this.numerals = new LinkedHashMap<>();
 
-        numerals.put(1, "I");
-        numerals.put(4, "IV");
-        numerals.put(5, "V");
-        numerals.put(9, "IX");
-        numerals.put(10, "X");
-        numerals.put(40, "XL");
         numerals.put(50, "L");
+        numerals.put(40, "XL");
+        numerals.put(10, "X");
+        numerals.put(9, "IX");
+        numerals.put(5, "V");
+        numerals.put(4, "IV");
+        numerals.put(1, "I");
     }
 
     public String convert(int arabic) {
 
         String roman = "";
 
-        if(arabic >= 50) {
-            arabic -= 50;
-            roman += numerals.get(50);
-        }
+        Iterator i = numerals.entrySet().iterator();
 
-        if(arabic >= 40) {
-            arabic -= 40;
-            roman += numerals.get(40);
-        }
+       while (i.hasNext()){
+           Map.Entry KeyValuePair = (Map.Entry) i.next();
+           int numeral = (int)KeyValuePair.getKey();
+           String value = (String)KeyValuePair.getValue();
 
-        if(arabic >= 10) {
-            arabic -= 10;
-            roman += numerals.get(10);
-        }
-
-        if(arabic == 9) {
-            arabic -= 9;
-            roman += numerals.get(9);
-        }
-
-        if(arabic >= 5) {
-            arabic -= 5;
-            roman += numerals.get(5);
-        }
-
-        if(arabic == 4) {
-            arabic -= 4;
-            roman += numerals.get(4);
-        }
+           if(arabic >= numeral) {
+               roman += value;
+               arabic -= numeral;
+           }
+       }
 
         if(arabic > 0 && arabic <= 3)
             roman = getOnes(arabic, roman);
