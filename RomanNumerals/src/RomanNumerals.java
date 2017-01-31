2ef2309714ec40ff0c59d16b5309800d3e1a32f2
EA <emashliles@gmail.com>
2017-01-31 15:07:06 +0000

    Replace magic strings with HashMap.

--- RomanNumerals/src/RomanNumerals.java
+++ RomanNumerals/src/RomanNumerals.java
@@ -1,4 +1,19 @@
+import java.util.HashMap;
+
 public class RomanNumerals {
+
+    private HashMap<Integer, String> numerals;
+
+    public RomanNumerals() {
+        this.numerals = new HashMap<>();
+        numerals.put(1, "I");
+        numerals.put(4, "IV");
+        numerals.put(5, "V");
+        numerals.put(9, "IX");
+        numerals.put(10, "X");
+        numerals.put(40, "XL");
+        numerals.put(50, "L");
+    }
 
     public String convert(int arabic) {
 
@@ -6,32 +21,32 @@ public class RomanNumerals {
 
         if(arabic >= 50){
             arabic -= 50;
-            roman += "L";
+            roman += numerals.get(50);
         }
 
         if(arabic >= 40){
             arabic -= 40;
-            roman += "XL";
+            roman += numerals.get(40);
         }
 
         if(arabic >= 10) {
             arabic -= 10;
-            roman += "X";
+            roman += numerals.get(10);
         }
 
         if(arabic == 9) {
             arabic -= 9;
-            roman += "IX";
+            roman += numerals.get(9);
         }
 
         if(arabic >= 5) {
             arabic -= 5;
-            roman = "V";
+            roman += numerals.get(5);
         }
 
         if(arabic == 4) {
             arabic -= 4;
-            roman += "IV";
+            roman += numerals.get(4);
         }
 
         if(arabic > 0 && arabic <= 3)
@@ -41,7 +56,7 @@ public class RomanNumerals {
     }
 
     private String getOnes(int i, String roman) {
-        roman = roman + new String(new char[i]).replace("\0", "I");
+        roman = roman + new String(new char[i]).replace("\0", numerals.get(1));
         return roman;
     }
 }
