3b9b973f3623460007760aef67d42347d05b335d
EA <emashliles@gmail.com>
2017-02-08 13:02:24 +0000

    Attempt 2 - no Iterator.

--- RomanNumerals/src/RomanNumerals.java
+++ RomanNumerals/src/RomanNumerals.java
@@ -1,9 +1,36 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RomanNumerals {
 
 
-    public static String convert(int i) {
-        throw new NotImplementedException();
+    public static String convert(int arabic) {
+
+        String roman = "";
+
+        Map<Integer, String> conversions = new LinkedHashMap();
+
+        conversions.put(100, "C");
+        conversions.put(50, "L");
+        conversions.put(10, "X");
+        conversions.put(9, "IX");
+        conversions.put(5, "V");
+        conversions.put(4, "IV");
+        conversions.put(1, "I");
+
+        for(Map.Entry conversion : conversions.entrySet()) {
+
+            Integer value = (Integer) conversion.getKey();
+            String numeral = (String) conversion.getValue();
+
+            while (value <= arabic) {
+                roman += numeral;
+                arabic -= value;
+            }
+        }
+
+        return roman;
     }
 }
--- RomanNumerals/test/RomanNumeralsTest.java
+++ RomanNumerals/test/RomanNumeralsTest.java
@@ -5,9 +5,63 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class RomanNumeralsTest {
 
     @Test
-    public void Converts0(){
+    public void converts0(){
         String roman = RomanNumerals.convert(0);
         assertEquals("", roman);
     }
 
+    @Test
+    void converts1() {
+        assertEquals("I",RomanNumerals.convert(1));
+    }
+
+    @Test
+    void converts2() {
+        assertEquals("II", RomanNumerals.convert(2));
+    }
+
+    @Test
+    void converts3() {
+        assertEquals("III", RomanNumerals.convert(3));
+    }
+
+    @Test
+    void converts4() {
+        assertEquals("IV", RomanNumerals.convert(4));
+    }
+
+    @Test
+    void converts5() {
+        assertEquals("V", RomanNumerals.convert(5));
+    }
+
+    @Test
+    void converts6() {
+        assertEquals("VI", RomanNumerals.convert(6));
+    }
+
+    @Test
+    void converts9() {
+        assertEquals("IX", RomanNumerals.convert(9));
+    }
+
+    @Test
+    void converts10() {
+        assertEquals("X", RomanNumerals.convert(10));
+    }
+
+    @Test
+    void converts14() {
+        assertEquals("XIV", RomanNumerals.convert(14));
+    }
+
+    @Test
+    void converts50() {
+        assertEquals("L", RomanNumerals.convert(50));
+    }
+
+    @Test
+    void converts100() {
+        assertEquals("C", RomanNumerals.convert(100));
+    }
 }
